911c946d0d4087fbbda1b377862bca99cfbde9d1
María Jesús Fuenzalida Serrano <maria.fuenzalida.serrano@gmail.com>
2020-03-10 16:58:09 -0300

    Segundo avance con funciones JS y estilos CSS listos

--- controllers/users.js
+++ controllers/users.js
@@ -1,43 +1,23 @@
-var User = require('../models/users');
-exports.user_create = function (req, res, next, callback) {
-    console.log("entrar al controlador");
+// Función registro de usuarios
+var User  = require('../models/users');
+var bcrypt   = require('bcryptjs');
+exports.user_create = function(req, res, next) {
     if (req.body) {
-        console.log("Datos ingresados correctamente");
-        let items = req.body;
-        User.create(items, function (err, newUsers) {
-            console.log("Se está creando el usuario");
-            if (err) return res.json({ error: err });
-            console.log("listo");
-            res.json(newUsers)
+        let items = req.body
+        User.create(items, function(err, newUsers){
+            if(err) return res.json({ error: err });
+            // return res.json(newUsers);
+            res.redirect("/");
         });
     } else {
-        res.json({
-            status: 'ERROR', message: 'Todos los Campos son OBLIGATORIOS'}); //mandar mensaje de error
+        return res.json({status: 'ERROR', message: 'Todos los Campos son OBLIGATORIOS'}); //opcional mandar un mensaje de error
     }
-}
+  }
 
 // Función agregar usuarios en la lista
 exports.list_user = function(req, res, next) {
     User.find({}, (err, data) => {
         if(err) return res.json({ error:err });
-        res.render('index', { title: 'Express 2', list: data });
+        res.render('index', { title: 'Formulario', list: data });
     });
-}
-
-// Función cambiar Ñ por doble N
-function cambiarLetra(arreglo) {
-
-    arreglo.map(function (arreglo) {
-
-        if (arreglo.name.includes('\u00f1')) {
-            let nombre = arreglo.name;
-            arreglo.name = nombre.replace(new RegExp("\u00f1", 'gi'), "n");// Poner nn
-        }
-        if (arreglo.lastname.includes('\u00f1')) {
-            let apellido = arreglo.lastname;
-            arreglo.lastname = apellido.replace(new RegExp("\u00f1", 'gi'), "n");// Poner nn
-        }
-    });
-    
-    return arreglo;  
 }
--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,6 @@ var router = express.Router();
 /* GET users listing. */
 router.post('/', function(req, res, next) {
   userController.user_create(req, res, next, ()=> res.redirect('/'));
-  userController.user_create(req, res, next);
 });
 
 module.exports = router;
